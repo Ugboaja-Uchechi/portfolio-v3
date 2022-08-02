5b8b2d1bd94fb5365b0860c3f42ced7ba2539833
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-08-03 00:43:30 +0100

    Style about page

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,6 @@ background-color: ${props => props.theme.body};
 width: 100vw;
 height: 100vh;
 position: relative;
-display: flex;
-justify-content: space-evenly;
-align-items: center;
 overflow: hidden;
 `
 
@@ -37,6 +34,27 @@ img {
 }
 `
 
+const Main = styled.div`
+border: 2px solid ${props => props.theme.text};
+color: ${props => props.theme.text};
+padding: 2rem;
+width: 50vw;
+height: 60vh;
+z-index: 3;
+line-height: 1.5;
+
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(0.6rem + 1vw);
+backdrop-filter: blur(4px);
+position: absolute;
+left: calc(5rem + 5vw);
+top: 10rem;
+font-family: 'Ubuntu Mono', monospace;
+font-style: italic;
+`
+
 const About = () => {
   return (
     <ThemeProvider theme={DarkTheme}>
@@ -51,9 +69,11 @@ const About = () => {
         </SpaceMan>
 
         <Main>
-          I'm a front-end developer located in India. I love to create simple yet beautiful websites with great user experience.
+          I'm a front-end developer located in Nigeria. I love to create simple yet beautiful websites with great user experience.
+          <br /><br />
 
           I'm interested in the whole frontend stack Like trying new things and building great projects. I'm an independent freelancer and blogger. I love to write blogs and read books.
+          <br /><br />
 
           I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.
         </Main>
