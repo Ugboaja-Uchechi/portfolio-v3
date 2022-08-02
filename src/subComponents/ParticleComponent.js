19f5277b0bd633439a32f002c8e000f8701e038a
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-08-02 23:39:48 +0100

    Add particles to skills page

--- src/subComponents/ParticleComponent.js
+++ src/subComponents/ParticleComponent.js
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { ParticlesProps } from "react-tsparticles";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+// Particles config file
+
+import configDark from "../config/particlesjs-config.json"
+import configLight from "../config/particlesjs-config-light.json"
 
 const Box = styled.div`
 position: absolute;
@@ -11,10 +16,99 @@ bottom: 0;
 z-index: 0;
 `
 
-const ParticleComponent = () => {
+const ParticleComponent = (props) => {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <Box>
-
+      {/* <Particles style={{position: "absolute", top: 0}} params={props.theme === "light" ? configLight : configDark} /> */}
+      <Particles style={{position: "absolute", top: 0}} params={props.theme === "light" ? configLight : configDark}
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#0d47a1",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 6,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+          />
     </Box>
   )
 }
