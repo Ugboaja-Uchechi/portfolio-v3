35dce0b472032df7c856420077607afdffa5b9f2
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-08-02 22:21:01 +0100

    Style skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,41 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { LightTheme } from "./Theme";
+
+const Box =styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height: 100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+`
+
+const Main =styled.div`
+border: 2px solid ${props => props.theme.text};
+color: ${props => props.theme.text};
+background-color: ${props => props.theme.body};
+padding: 2rem;
+width: 30vw;
+height: 60vh;
+z-index: 3;
+line-height: 1.5;
+`
 
 const Skill = () => {
   return (
-    <>
-      <h1>Skill Component/Page</h1>
-    </>
+    <ThemeProvider theme={LightTheme}>
+      <Box>
+        <Main>
+          Skill Component/Page
+        </Main>
+        <Main>
+          Skill Component2/Page
+        </Main>
+
+      </Box>
+    </ThemeProvider>
   )
 }
 
