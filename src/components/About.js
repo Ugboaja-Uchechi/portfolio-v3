38d61899acae30a6da579a48149cc9bd6573dc22
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-08-03 00:27:47 +0100

    Style about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,65 @@
 import React from "react";
+import styled, { keyframes, ThemeProvider } from "styled-components";
+import { DarkTheme } from "./Theme";
+import Astronaut from "../assests/images/spaceman.png"
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
+
+const Box =styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height: 100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+overflow: hidden;
+`
+
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+`
+
+const SpaceMan = styled.div`
+position: absolute;
+top: 10%;
+right: 5%;
+width: 20vw;
+animation: ${float} 4s ease infinite;
+
+img {
+  width: 100%;
+  height: auto;
+}
+`
 
 const About = () => {
   return (
-    <>
-      <h2>About Component/Page</h2>
-    </>
+    <ThemeProvider theme={DarkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <ParticleComponent theme="dark" />
+
+        <SpaceMan>
+          <img src={Astronaut} alt="astronaut" />
+        </SpaceMan>
+
+        <Main>
+          I'm a front-end developer located in India. I love to create simple yet beautiful websites with great user experience.
+
+          I'm interested in the whole frontend stack Like trying new things and building great projects. I'm an independent freelancer and blogger. I love to write blogs and read books.
+
+          I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.
+        </Main>
+
+      </Box>
+    </ThemeProvider>
   )
 }
 
