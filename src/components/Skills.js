8b9fd8ebcf96bc1d8d420e5ba0906ab9e6e958b9
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-08-02 23:10:12 +0100

    Style skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,11 @@
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { LightTheme } from "./Theme";
+import { Design, Develope } from "./AllSvgs";
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
 
 const Box =styled.div`
 background-color: ${props => props.theme.body};
@@ -21,17 +26,92 @@ width: 30vw;
 height: 60vh;
 z-index: 3;
 line-height: 1.5;
+font-family: "Ubuntu Mono", monospace;
+display: flex;
+justify-content: space-between;
+flex-direction: column;
+
+&:hover {
+  color: ${props => props.theme.body};
+  background-color: ${props => props.theme.text};
+}
+`
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1em + 1vw);
+
+  ${Main}:hover & {
+    &>* {
+      fill: ${props => props.theme.body};
+    }
+  }
+  &>*:first-child {
+    margin-right: 1rem;
+  }
+`
+
+const Description = styled.div`
+color: ${props => props.theme.text};
+font-size: calc(0.6em + 1vw);
+padding: 0.5rem 0;
+
+strong {
+  margin-bottom: 1rem;
+  text-transform: uppercase;
+}
+
+p {
+  margin-left: 2rem;
+}
+
+${Main}:hover & {
+    color: ${props => props.theme.body};
+}
+
 `
 
 const Skill = () => {
   return (
     <ThemeProvider theme={LightTheme}>
       <Box>
+        <LogoComponent theme="light" />
+        <SocialIcons theme="light" />
+        <PowerButton />
+        <ParticleComponent theme="light" />
         <Main>
-          Skill Component/Page
+          <Title>
+            <Design width={40} height={40} /> Designer
+          </Title>
+          <Description>
+            I love to create design which speaks, Keep it clean, minimal and simple.
+          </Description>
+          <Description>
+            <strong>Skills</strong>
+            <p>Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.</p>
+          </Description>
+          <Description>
+            <strong>Tools</strong>
+            <p>VScode, Github, Codepen etc.</p>
+          </Description>
         </Main>
         <Main>
-          Skill Component2/Page
+        <Title>
+            <Develope width={40} height={40} /> Frontend Developer
+          </Title>
+          <Description>
+            I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
+          </Description>
+          <Description>
+            <strong>Skills</strong>
+            <p>Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.</p>
+          </Description>
+          <Description>
+            <strong>Tools</strong>
+            <p>VScode, Github, Codepen etc.</p>
+          </Description>
         </Main>
 
       </Box>
---
+++ src/subComponents/ParticleComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import { ParticlesProps } from "react-tsparticles";
+
+const Box = styled.div`
+position: absolute;
+top: 0;
+right: 0;
+left: 0;
+bottom: 0;
+z-index: 0;
+`
+
+const ParticleComponent = () => {
+  return (
+    <Box>
+
+    </Box>
+  )
+}
+
+export default ParticleComponent;
